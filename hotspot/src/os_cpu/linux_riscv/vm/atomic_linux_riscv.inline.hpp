ae1aa5fbe82fe47c87c97931053afb50aca30480
sendaoYan <yansendao.ysd@alibaba-inc.comn>
2024-07-07 17:47:01 +0800

    copy from atomic_linux_aarch64.inline.hpp to atomic_linux_riscv.inline.hpp, to fix compile error: atomic.hpp:88:26: error: inline function ‘static jint Atomic::cmpxchg(jint, volatile jint*, jint)’ used but never defined

--- hotspot/src/os_cpu/linux_riscv/vm/atomic_linux_riscv.inline.hpp
+++ hotspot/src/os_cpu/linux_riscv/vm/atomic_linux_riscv.inline.hpp
@@ -39,6 +39,19 @@
 #define READ_MEM_BARRIER  __atomic_thread_fence(__ATOMIC_ACQUIRE);
 #define WRITE_MEM_BARRIER __atomic_thread_fence(__ATOMIC_RELEASE);
 
+inline void Atomic::store    (jbyte    store_value, jbyte*    dest) { *dest = store_value; }
+inline void Atomic::store    (jshort   store_value, jshort*   dest) { *dest = store_value; }
+inline void Atomic::store    (jint     store_value, jint*     dest) { *dest = store_value; }
+inline void Atomic::store_ptr(intptr_t store_value, intptr_t* dest) { *dest = store_value; }
+inline void Atomic::store_ptr(void*    store_value, void*     dest) { *(void**)dest = store_value; }
+
+inline void Atomic::store    (jbyte    store_value, volatile jbyte*    dest) { *dest = store_value; }
+inline void Atomic::store    (jshort   store_value, volatile jshort*   dest) { *dest = store_value; }
+inline void Atomic::store    (jint     store_value, volatile jint*     dest) { *dest = store_value; }
+inline void Atomic::store_ptr(intptr_t store_value, volatile intptr_t* dest) { *dest = store_value; }
+inline void Atomic::store_ptr(void*    store_value, volatile void*     dest) { *(void* volatile *)dest = store_value; }
+
+
 inline jint Atomic::add(jint add_value, volatile jint* dest)
 {
  return __sync_add_and_fetch(dest, add_value);
@@ -49,14 +62,19 @@ inline void Atomic::inc(volatile jint* dest)
  add(1, dest);
 }
 
-inline intptr_t Atomic::add_ptr(intptr_t add_value, volatile intptr_t* dest)
+inline void Atomic::inc_ptr(volatile void* dest)
 {
- return __sync_add_and_fetch(dest, add_value);
+ add_ptr(1, dest);
 }
 
-inline void* Atomic::add_ptr(intptr_t add_value, volatile void* dest)
+inline void Atomic::dec (volatile jint* dest)
 {
-  return (void *) add_ptr(add_value, (volatile intptr_t *) dest);
+ add(-1, dest);
+}
+
+inline void Atomic::dec_ptr(volatile void* dest)
+{
+ add_ptr(-1, dest);
 }
 
 inline jint Atomic::xchg (jint exchange_value, volatile jint* dest)
@@ -70,6 +88,34 @@ inline void* Atomic::xchg_ptr(void* exchange_value, volatile void* dest)
 {
   return (void *) xchg_ptr((intptr_t) exchange_value,
                            (volatile intptr_t*) dest);
+}
+
+inline jint Atomic::cmpxchg (jint exchange_value, volatile jint* dest, jint compare_value)
+{
+ return __sync_val_compare_and_swap(dest, compare_value, exchange_value);
+}
+
+inline void Atomic::store (jlong store_value, jlong* dest) { *dest = store_value; }
+inline void Atomic::store (jlong store_value, volatile jlong* dest) { *dest = store_value; }
+
+inline intptr_t Atomic::add_ptr(intptr_t add_value, volatile intptr_t* dest)
+{
+ return __sync_add_and_fetch(dest, add_value);
+}
+
+inline void* Atomic::add_ptr(intptr_t add_value, volatile void* dest)
+{
+  return (void *) add_ptr(add_value, (volatile intptr_t *) dest);
+}
+
+inline void Atomic::inc_ptr(volatile intptr_t* dest)
+{
+ add_ptr(1, dest);
+}
+
+inline void Atomic::dec_ptr(volatile intptr_t* dest)
+{
+ add_ptr(-1, dest);
 }
 
 inline intptr_t Atomic::xchg_ptr(intptr_t exchange_value, volatile intptr_t* dest)
@@ -96,18 +142,6 @@ inline void* Atomic::cmpxchg_ptr(void* exchange_value, volatile void* dest, void
                               (intptr_t) compare_value);
 }
 
-inline void Atomic::store    (jbyte    store_value, jbyte*    dest) { *dest = store_value; }
-inline void Atomic::store    (jshort   store_value, jshort*   dest) { *dest = store_value; }
-inline void Atomic::store    (jint     store_value, jint*     dest) { *dest = store_value; }
-inline void Atomic::store_ptr(intptr_t store_value, intptr_t* dest) { *dest = store_value; }
-inline void Atomic::store_ptr(void*    store_value, void*     dest) { *(void**)dest = store_value; }
-inline void Atomic::store    (jbyte    store_value, volatile jbyte*    dest) { *dest = store_value; }
-inline void Atomic::store    (jshort   store_value, volatile jshort*   dest) { *dest = store_value; }
-inline void Atomic::store    (jint     store_value, volatile jint*     dest) { *dest = store_value; }
-inline void Atomic::store_ptr(intptr_t store_value, volatile intptr_t* dest) { *dest = store_value; }
-inline void Atomic::store_ptr(void*    store_value, volatile void*     dest) { *(void* volatile *)dest = store_value; }
-inline void Atomic::store (jlong store_value, jlong* dest) { *dest = store_value; }
-inline void Atomic::store (jlong store_value, volatile jlong* dest) { *dest = store_value; }
 inline jlong Atomic::load(volatile jlong* src) { return *src; }
 
 #endif // OS_CPU_LINUX_RISCV_ATOMIC_LINUX_RISCV_INLINE_HPP
