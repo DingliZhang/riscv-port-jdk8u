362702545dd72d0ed157562f14e9ffabb033ee95
sendaoYan <yansendao.ysd@alibaba-inc.comn>
2024-07-13 17:51:31 +0800

    revert JDK-8151268, to fix compile error: c1_LIRGenerator_riscv.cpp:958:6: error: no declaration matches ‘void LIRGenerator::do_vectorizedMismatch(Intrinsic*)’

--- hotspot/src/cpu/riscv/vm/c1_LIRGenerator_riscv.cpp
+++ hotspot/src/cpu/riscv/vm/c1_LIRGenerator_riscv.cpp
@@ -955,10 +955,6 @@ void LIRGenerator::do_update_CRC32(Intrinsic* x) {
   ShouldNotReachHere();
 }
 
-void LIRGenerator::do_vectorizedMismatch(Intrinsic* x) {
-  fatal("vectorizedMismatch intrinsic is not implemented on this platform");
-}
-
 // _i2l, _i2f, _i2d, _l2i, _l2f, _l2d, _f2i, _f2l, _f2d, _d2i, _d2l, _d2f
 // _i2b, _i2c, _i2s
 void LIRGenerator::do_Convert(Convert* x) {
