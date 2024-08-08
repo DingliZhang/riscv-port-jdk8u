49d7c1c1e55c0563ddf1d89d2ed6842aacba5794
sendaoYan <yansendao.ysd@alibaba-inc.com>
2024-08-08 20:57:30 +0800

    fix typo bug after revert JDK-8276563, to fix compile error: register_riscv.cpp:47:3: error: ‘sconst’ was not declared in this scope #172

--- hotspot/src/cpu/riscv/vm/register_riscv.cpp
+++ hotspot/src/cpu/riscv/vm/register_riscv.cpp
@@ -44,7 +44,7 @@ const char* RegisterImpl::name() const {
 }
 
 const char* FloatRegisterImpl::name() const {
-  sconst char* names[number_of_registers] = {
+  const char* names[number_of_registers] = {
     "f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7",
     "f8", "f9", "f10", "f11", "f12", "f13", "f14", "f15",
     "f16", "f17", "f18", "f19", "f20", "f21", "f22", "f23",
