10f65894bebf8e3b8403123074a7add81198bd53
sendaoYan <yansendao.ysd@alibaba-inc.com>
2024-08-30 15:19:32 +0800

    revert JDK-8132510 remainder(delete te MacroAssembler::get_thread(Register thread) implementation), to fix link error: macroAssembler_riscv.cpp:2960: multiple definition of `MacroAssembler::get_thread(RegisterImpl*)

--- hotspot/src/os_cpu/linux_riscv/vm/os_linux_riscv.cpp
+++ hotspot/src/os_cpu/linux_riscv/vm/os_linux_riscv.cpp
@@ -199,7 +199,6 @@ JVM_handle_linux_signal(int sig,
                         int abort_if_unrecognized) {
   ucontext_t* uc = (ucontext_t*) ucVoid;
 
-  // Thread* t = Thread::current_or_null_safe();
   Thread* t = ThreadLocalStorage::get_thread_slow();
 
   // Must do this before SignalHandlerMark, if crash protection installed we will longjmp away
